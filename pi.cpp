ff3d9289a002ce681a6588e3b035320b6473634b
nan-mu <mu.nan@88.com>
2022-09-30 22:39:17 +0800

    修好了，但仍不知道之前的版本有什么问题，可能和long long int有关？

--- pi.cpp
+++ pi.cpp
@@ -6,19 +6,13 @@
 using namespace std;
 
 int main() {
-	long long int x = 0, y = 0;							//预计生成两个12位数表示投针坐标
-	long long int zx = 274877906944, zy = 274877906944; //设定正方形的范围
-	long long int r = 137438953472;						//设定圆的半径
-	double goal = 0;									//投中的数量
-	double cNum = 100000000;							//计算次数
-	srand((int)time(0));								//随机数种子
-	for (double i = 0; i < cNum; i++) {					//开投！
-		x = (long long)(rand()) * 8388864;
-		y = (long long)(rand()) * 8388864;
-		if (((x - r) * (x - r) + (y - r) * (y - r)) < r)
-			goal += 1;
+    srand((int)time(0));
+	double x = 0.0, y = 0.0, times = 500000000, goal = 0;
+	for (int i = 0; i < times; i++) {
+		x = rand()/(double)RAND_MAX;
+        y = rand()/(double)RAND_MAX;
+        if (((x - 0.5)*(x - 0.5) + (y - 0.5)*(y - 0.5)) <= 0.25)goal += 1;
 	}
 
-	printf("goal=%llf\n", goal);
-	printf("PI=%lf", ((4 * goal) / cNum));
+	printf("PI=%7f", ((4 * goal) / times));
 }
