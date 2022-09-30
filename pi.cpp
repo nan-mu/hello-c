67ead02e1fa9c13970ed0b740e291421c8d61ec6
nan-mu <mu.nan@88.com>
2022-09-30 22:50:27 +0800

    修改了单词的拼写错误 完善了注释和README

--- pi.cpp
+++ pi.cpp
@@ -6,13 +6,14 @@
 using namespace std;
 
 int main() {
-    srand((int)time(0));
-	double x = 0.0, y = 0.0, times = 500000000, goal = 0;
-	for (int i = 0; i < times; i++) {
-		x = rand()/(double)RAND_MAX;
-        y = rand()/(double)RAND_MAX;
-        if (((x - 0.5)*(x - 0.5) + (y - 0.5)*(y - 0.5)) <= 0.25)goal += 1;
+	srand((int)time(0));								  //设置随机数种子
+	double x = 0.0, y = 0.0, times = 500000000, goal = 0; //初始化变量
+	for (int i = 0; i < times; i++) {					  //开投
+		x = rand() / (double)RAND_MAX;
+		y = rand() / (double)RAND_MAX;
+		if (((x - 0.5) * (x - 0.5) + (y - 0.5) * (y - 0.5)) <= 0.25) //判断是否投在圆内
+			goal += 1;
 	}
 
-	printf("PI=%7f", ((4 * goal) / times));
+	printf("PI=%7f", ((4 * goal) / times)); //计算PI并输出
 }
